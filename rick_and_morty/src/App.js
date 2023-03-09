3fd941f6d7bdac6c649fbfa2ba58cd571e5ad557
Jonavoe <voeffray.jonathan@gmail.com>
2023-03-09 10:20:29 -0300

    Cambio de API a API henry

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -7,8 +7,11 @@ import React, { useState } from 'react';
 function App() {
   const [characters, setCharacters] = useState([]);
 
-  function onSearch(id) {
-    fetch(`https://rickandmortyapi.com/api/character/${id}`)
+  const onSearch = id => {
+    // fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    const URL_BASE = 'https://be-a-rym.up.railway.app/api';
+    const KEY = '1ffccaa0d688.4235080dc23aac8bb293';
+    fetch(`${URL_BASE}/character/${id}?key=${KEY}`)
       .then(response => response.json())
       .then(data => {
         if (data.id) {
@@ -24,7 +27,7 @@ function App() {
           window.alert('No hay personajes con ese ID');
         }
       });
-  }
+  };
 
   function removeCharacters(id) {
     setCharacters(characters => characters.filter(c => c.id !== id));
